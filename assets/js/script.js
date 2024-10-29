318476ec5eb1d80e121a49f6dc112fbb5b07e691
Seema-476 <simmybishnoi29085@gmail.com>
2024-10-29 19:51:26 +0530

    workup page done

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// menu-icon
+function opennav() {
+    document.getElementById("navlist").classList.toggle("start-0");
+    document.querySelector(".menuicon").classList.toggle("cross");
+    document.body.classList.toggle("overflow-hidden");
+}
+
