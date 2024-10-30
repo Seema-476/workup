307a4452ec2ed801358266ebb7976b9f07677275
Seema-476 <simmybishnoi29085@gmail.com>
2024-10-30 22:33:13 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 function opennav() {
     document.getElementById("navlist").classList.toggle("start-0");
     document.querySelector(".menuicon").classList.toggle("cross");
-    document.body.classList.toggle("overflow-hidden");
+    document.body.classList.toggle(".hidden-overflow");
 }
 
+// scroll top btn 
+const topbtn = document.querySelector(".topbtn")
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topbtn.classList.add("show")
+    } else {
+        topbtn.classList.remove("show");
+    }
+})
+topbtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
